c17870e687404e33ee7ae5c159f9834aebb91223
skhacker105 <skhacker105@gmail.com>
2026-03-24 16:59:33 +0530

    new commit

---
+++ app/src/main/java/com/saurabhkumarbusiness/societymanagement/MainActivity.java
@@ -0,0 +1,39 @@
+package com.saurabhkumarbusiness.societymanagement;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    // Handle notification click when app is launched from notification
+    handleNotificationIntent(getIntent());
+  }
+
+  @Override
+  public void onNewIntent(Intent intent) {
+    super.onNewIntent(intent);
+    setIntent(intent);
+    handleNotificationIntent(intent);
+  }
+
+  private void handleNotificationIntent(Intent intent) {
+    // This method is called when app is opened from notification
+    // The Capacitor Pushx  Notifications plugin will automatically
+    // handle the notification data and trigger the appropriate listeners
+    // in your Angular/JavaScript code
+
+    if (intent != null && intent.getExtras() != null) {
+      // Log for debugging
+      System.out.println("App opened with intent extras: " + intent.getExtras().toString());
+
+      // The PushNotifications plugin in JavaScript will receive this
+      // and trigger the 'pushNotificationActionPerformed' listener
+    }
+  }
+}
